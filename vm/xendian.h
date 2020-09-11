0bc70782eb999fd5dd344b6611316d7cec706ed1
debugfan <debugfanchin@gmail.com>
2020-09-11 11:06:45 +0800

    Replace with endian.h from glibc 2.12

--- vm/xendian.h
+++ vm/xendian.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1992-2018 Free Software Foundation, Inc.
+/* Copyright (C) 1992, 1996, 1997, 2000, 2008 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -12,8 +12,9 @@
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.  */
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
 
 #ifndef	_ENDIAN_H
 #define	_ENDIAN_H	1
@@ -41,7 +42,7 @@
 # define __FLOAT_WORD_ORDER __BYTE_ORDER
 #endif
 
-#ifdef	__USE_MISC
+#ifdef	__USE_BSD
 # define LITTLE_ENDIAN	__LITTLE_ENDIAN
 # define BIG_ENDIAN	__BIG_ENDIAN
 # define PDP_ENDIAN	__PDP_ENDIAN
@@ -55,41 +56,39 @@
 #endif
 
 
-#if defined __USE_MISC && !defined __ASSEMBLER__
+#ifdef __USE_BSD
 /* Conversion interfaces.  */
 # include <bits/byteswap.h>
-# include <bits/uintn-identity.h>
 
 # if __BYTE_ORDER == __LITTLE_ENDIAN
 #  define htobe16(x) __bswap_16 (x)
-#  define htole16(x) __uint16_identity (x)
+#  define htole16(x) (x)
 #  define be16toh(x) __bswap_16 (x)
-#  define le16toh(x) __uint16_identity (x)
+#  define le16toh(x) (x)
 
 #  define htobe32(x) __bswap_32 (x)
-#  define htole32(x) __uint32_identity (x)
+#  define htole32(x) (x)
 #  define be32toh(x) __bswap_32 (x)
-#  define le32toh(x) __uint32_identity (x)
+#  define le32toh(x) (x)
 
 #  define htobe64(x) __bswap_64 (x)
-#  define htole64(x) __uint64_identity (x)
+#  define htole64(x) (x)
 #  define be64toh(x) __bswap_64 (x)
-#  define le64toh(x) __uint64_identity (x)
-
+#  define le64toh(x) (x)
 # else
-#  define htobe16(x) __uint16_identity (x)
+#  define htobe16(x) (x)
 #  define htole16(x) __bswap_16 (x)
-#  define be16toh(x) __uint16_identity (x)
+#  define be16toh(x) (x)
 #  define le16toh(x) __bswap_16 (x)
 
-#  define htobe32(x) __uint32_identity (x)
+#  define htobe32(x) (x)
 #  define htole32(x) __bswap_32 (x)
-#  define be32toh(x) __uint32_identity (x)
+#  define be32toh(x) (x)
 #  define le32toh(x) __bswap_32 (x)
 
-#  define htobe64(x) __uint64_identity (x)
+#  define htobe64(x) (x)
 #  define htole64(x) __bswap_64 (x)
-#  define be64toh(x) __uint64_identity (x)
+#  define be64toh(x) (x)
 #  define le64toh(x) __bswap_64 (x)
 # endif
 #endif
